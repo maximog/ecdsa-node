77b5720fdd0eb06d8e7ebce2fca597c90a9a41df
Maximo Gijon <maximo.gijon@gmail.com>
2022-11-25 16:07:33 -0300

    add state, initialize with some values

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,10 @@ import { useState } from "react";
 
 function App() {
   const [balance, setBalance] = useState(0);
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useState("0xd78f4fba25ccd9086486b371324f2c3dc6df96e4");
+  const [recipient, setRecipient] = useState("0xa311db737ace2f48de1b949051d1cc2e975a258c");
+  const [amount, setAmount] = useState(0);
+  const [nonce, setNonce] = useState(0);
 
   return (
     <div className="app">
