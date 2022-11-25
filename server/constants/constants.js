ef2bda28c0169f2fe78b0944bb72fd58ecf3ef76
Maximo Gijon <maximo.gijon@gmail.com>
2022-11-25 16:22:15 -0300

    add 3 test accounts with address, priv key, pub key

---
+++ server/constants/constants.js
@@ -0,0 +1,23 @@
+const ACCOUNTS = [
+  {
+    address: "0xd78f4fba25ccd9086486b371324f2c3dc6df96e4",
+    privateKey: "aa8659282571af927953dc594b40a5cb8e4646fe86a0082783d01bf0797744c7",
+    publicKey:
+      "045be383a27088fd009d71b3aa87a3e69ab15c1c1b597af9c3167f02dd20778bb4e4e0eb3a6da7522809d5852f262eddbef6b4c42645af0d9668c9374dda485f84",
+  },
+
+  {
+    address: "0xa311db737ace2f48de1b949051d1cc2e975a258c",
+    privateKey: "ac08d5d053279d0fdda847e9d535749153756e9e97dd1c404a8b32af70cff619",
+    publicKey:
+      "04f19acac4f6d299ce9ac3fc048bdfe32c2eb6532d3b309af4a0b7317407c1a8a85dbc263bfbd1b982c50dc2d80764aa60420f104cb04130292240631ec5a4011f",
+  },
+  {
+    address: "0x956d6f868c3f925e8ffb118fe21714c3502dc48b",
+    privateKey: "fc55c49a2473d1971629b17f96519a43eecf4885e24eae1f5591d16864d1ba92",
+    publicKey:
+      "04d149bf2a1cffb5a266a6889e6b300a1138d02a0d9b52a753979cfc820a2775f40ddaaed8009c0e94ebabf7f99b7efce138d516d8de6a8d691da337fd3a47cc0f",
+  },
+];
+
+module.exports = ACCOUNTS;
