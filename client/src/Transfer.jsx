8b2960ee268adfce6303a2a2a15ba8fba15f9dd7
Maximo Gijon <maximo.gijon@gmail.com>
2022-11-25 16:20:04 -0300

    add signature field, props, set state on success

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import server from "./server";
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
 
-function Transfer({ address, setBalance }) {
-  const [sendAmount, setSendAmount] = useState("");
-  const [recipient, setRecipient] = useState("");
+function Transfer({
+  address,
+  setBalance,
+  recipient,
+  setRecipient,
+  amount,
+  setAmount,
+  nonce,
+  setNonce,
+}) {
+  const [signature, setSignature] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
@@ -11,14 +21,21 @@ function Transfer({ address, setBalance }) {
     evt.preventDefault();
 
     try {
+      // const msg = JSON.stringify({ amount: parseInt(amount), recipient, nonce });
+      // const hashedMsg = keccak256(utf8ToBytes(msg));
       const {
-        data: { balance },
+        data: { balance, nonce: newNonce, message },
       } = await server.post(`send`, {
         sender: address,
-        amount: parseInt(sendAmount),
         recipient,
+        nonce,
+        amount: parseInt(amount),
+        signature,
+        // hashedMsg: toHex(hashedMsg),
       });
       setBalance(balance);
+      setNonce(newNonce);
+      alert(message);
     } catch (ex) {
       alert(ex.response.data.message);
     }
@@ -30,11 +47,7 @@ function Transfer({ address, setBalance }) {
 
       <label>
         Send Amount
-        <input
-          placeholder="1, 2, 3..."
-          value={sendAmount}
-          onChange={setValue(setSendAmount)}
-        ></input>
+        <input placeholder="1, 2, 3..." value={amount} onChange={setValue(setAmount)}></input>
       </label>
 
       <label>
@@ -43,6 +56,15 @@ function Transfer({ address, setBalance }) {
           placeholder="Type an address, for example: 0x2"
           value={recipient}
           onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+
+      <label>
+        Signature
+        <input
+          placeholder="Insert the TX signature"
+          value={signature}
+          onChange={setValue(setSignature)}
         ></input>
       </label>
 
